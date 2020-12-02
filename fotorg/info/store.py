73255375881c0680a49261a0cc4b87aacc7db3f7
christoph <chr@buchetmann.de>
2020-12-02 10:20:20 +0100

    implement regular exifs and gps info

--- fotorg/info/store.py
+++ fotorg/info/store.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, func
+from sqlalchemy import Column, DateTime, Integer, String  # , ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
-from PIL.ExifTags import TAGS, GPSTAGS
 from typing import Dict
 from datetime import datetime
+from geopy.point import Point
 
 Base = declarative_base()
 
@@ -22,75 +22,140 @@ class Store:
 
     """
        
-        # GPSInfo = {1: 'N', 2: (44.0, 49.0, 20.06), 3: 'E', 4: (20.0, 24.0, 38.94), 5: b'\x00', 6: 89.0, 7: (10.0, 43.0, 54.37), 12: 'K', 13: 0.0, 16: 'T', 17: 246.48684210526315, 23: 'T', 24: 246.48684210526315, 29: '2016:06:16', 31: 65.0}
- 
+        # GPSInfo = {
+        1: 'N', 2: (44.0, 49.0, 20.06), 
+        3: 'E', 4: (20.0, 24.0, 38.94), 
+        5: b'\x00', 
+        6: 89.0, 
+        7: (10.0, 43.0, 54.37), 
+        12: 'K', 13: 0.0, 16: 'T', 17: 246.48684210526315, 23: 'T', 
+        24: 246.48684210526315, 29: '2016:06:16', 31: 65.0}
+     0: "GPSVersionID",
+    1: "GPSLatitudeRef",
+    2: "GPSLatitude",
+    3: "GPSLongitudeRef",
+    4: "GPSLongitude",
+    5: "GPSAltitudeRef",
+    6: "GPSAltitude",
+    7: "GPSTimeStamp",
+    
+
     """
 
-    @staticmethod
-    def __get_tag(name: str) -> str:
-        return next(filter(lambda x: TAGS[x] == name, TAGS), None)
+    @property
+    def image_description(self) -> str:
+        """010e	ImageDescription	Titel des Bildes und kurze Bildbeschreibung"""
+        return self.__data.get(0x010e, 'N/A')
+
+    @property
+    def artist(self) -> str:
+        """013B	Artist	Ersteller/Photograph"""
+        return self.__data.get(0x013B, 'N/A')
+
+    @property
+    def copyright(self) -> str:
+        """8298	Copyright	Urheberrechts-Information"""
+        return self.__data.get(0x8298, 'N/A')
+
+    @property
+    def user_comment(self) -> str:
+        """9286	UserComment	Allgemeiner Kommentar oder Bildbeschreibung. Unicode-fähig"""
+        return self.__data.get(0x9286, 'N/A')
+
+    @property
+    def metering_mode(self) -> int:
+        """9207	MeteringMode	Belichtungsmessverfahren Mittelwert/Spot/Multisegment"""
+        return int(self.__data.get(0x9207, 0))
+
+    @property
+    def iso_speed(self) -> int:
+        """8833	ISOSpeed	Empfindlichkeit des Bildsensors"""
+        return int(self.__data.get(0x8833, 0))
+
+    @property
+    def exposure_time(self) -> float:
+        """829A	ExposureTime	Belichtungszeit, e.g. 1/175"""
+        return float(self.__data.get(0x829A, 0.0))
+
+    @property
+    def f_number(self) -> str:
+        """829D	FNumber	Blendenzahl, e.g. f/2,2"""
+        return self.__data.get(0x8290, 'N/A')
+
+    @property
+    def exif_version(self) -> str:
+        """9000	ExifVersion	Version des Exif-Standards"""
+        return self.__data.get(0x9000, 'N/A')
+
+    @property
+    def make(self) -> str:
+        """010F	Make	Name des Kameraherstellers"""
+        return self.__data.get(0x010F, 'N/A')
 
     @property
     def camera_model(self) -> str:
-        tag = self.__get_tag('Model')
-        if tag is None:
-            return ''
-        return self.__data[tag]
+        """0110	Model	Name des Kamera-Modells"""
+        return self.__data.get(0x0110, 'N/A')
 
     @property
     def orientation(self) -> int:
-        tag = self.__get_tag('Orientation')
-        if tag is None:
-            return 0
-        return int(self.__data[tag])
+        """0112	Orientation	Bild-Ausrichtung Hochformat / Querformat"""
+        return int(self.__data.get(0x0112, 0))
 
     @property
     def resolution_unit(self) -> int:
-        tag = self.__get_tag('ResolutionUnit')
-        if tag is None:
-            return 0
-        return int(self.__data[tag])
+        """Optional resolution unit"""
+        return int(self.__data.get(0x0128, 0))
 
     @property
     def resolution_x(self) -> float:
-        tag = self.__get_tag('XResolution')
-        if tag is None:
-            return 0.0
-        return float(self.__data[tag])
+        """Optional resolution, x axis"""
+        return float(self.__data.get(0x011A, 0.0))
 
     @property
     def resolution_y(self) -> float:
-        tag = self.__get_tag('YResolution')
-        if tag is None:
-            return 0.0
-        return float(self.__data[tag])
+        """Optional resolution, x axis"""
+        return float(self.__data.get(0x011B, 0.0))
 
     @property
     def height(self) -> int:
-        tag = self.__get_tag('ExifImageHeight')
-        if tag is None:
-            return 0
-        return int(self.__data[tag])
+        """Optional image height"""
+        return int(self.__data.get(0xA003, 0))
 
     @property
     def width(self) -> int:
-        tag = self.__get_tag('ExifImageWidth')
-        if tag is None:
-            return 0
-        return int(self.__data[tag])
+        """Optional image width"""
+        return int(self.__data.get(0xA002, 0))
 
     @property
     def brightness(self) -> float:
-        tag = self.__get_tag('BrightnessValue')
-        if tag is None:
-            return 0.0
-        return float(self.__data[tag])
+        """Optional brightness"""
+        return float(self.__data.get(0x9203, 0.0))
 
     @property
     def created(self) -> datetime:
-        """# DateTimeOriginal = 2016:06:16 12:43:55"""
-        tag = self.__get_tag('DateTimeOriginal')
-        if tag is None:
-            return datetime(1970, 1, 1, 0, 0, 0, 0)
-        date_values = [int(x) for x in self.__data[tag].replace(' ', ':').split(':')]
+        """9003	DateTimeOriginal	Aufnahmedatum
+        # DateTimeOriginal = 2016:06:16 12:43:55"""
+        date_values = [int(x) for x in self.__data.get(0x9003, '1970:01:01 00:00:00').replace(' ', ':').split(':')]
         return datetime(date_values[0], date_values[1], date_values[2], date_values[3], date_values[4], date_values[5])
+
+    @property
+    def gps_info(self) -> Point:
+        """0x8825: "GPSInfo"""
+        geo_data = self.__data.get(0x8825, None)
+        if geo_data is None:
+            return Point()
+        latitude = Point.parse_degrees(geo_data[2][0],
+                                       geo_data[2][1],
+                                       geo_data[2][2],
+                                       geo_data[1])
+        longitude = Point.parse_degrees(geo_data[4][0],
+                                        geo_data[4][1],
+                                        geo_data[4][2],
+                                        geo_data[3])
+        alt_unit = 'km'
+        if str(geo_data[5]).lower() in ['m', 'mi', 'ft', 'nm', 'nmi']:
+            alt_unit = str(geo_data[5]).lower()
+        altitude = Point.parse_altitude(geo_data[6], alt_unit)
+        # GPSInfo = {1: 'N', 2: (44.0, 49.0, 20.06), 3: 'E', 4: (20.0, 24.0, 38.94), 5: b'\x00', 6: 89.0, 7: (10.0, 43.0, 54.37), 12: 'K', 13: 0.0, 16: 'T', 17: 246.48684210526315, 23: 'T', 24: 246.48684210526315, 29: '2016:06:16', 31: 65.0}
+        return Point(latitude=latitude, longitude=longitude, altitude=altitude)
--- test/test_info_store.py
+++ test/test_info_store.py
@@ -10,10 +10,9 @@ class TestInfoStore(unittest.TestCase):
         self.assertTrue(photo_org_store)
 
     def test_exif(self):
-        from PIL.ExifTags import TAGS, GPSTAGS
         from PIL import Image
         image = Image.open('test/test_folder/IMG_0564.JPG')
-        data = {34665: 204, 36864: b'0221', 37121: b'\x01\x02\x03\x00', 37377: 7.450526315789474,
+        """ {34665: 204, 36864: b'0221', 37121: b'\x01\x02\x03\x00', 37377: 7.450526315789474,
                 36867: '2016:06:16 12:43:55', 36868: '2016:06:16 12:43:55', 37378: 2.2750071245369052,
                 37379: 6.454077883908891, 37380: 0.0, 37383: 5, 37385: 24, 37386: 4.15, 40961: 1, 40962: 3264,
                 41989: 29, 41990: 0, 40963: 2448, 37521: '173', 37522: '173', 37396: (1631, 1223, 1795, 1077), 41495: 2,
@@ -25,10 +24,11 @@ class TestInfoStore(unittest.TestCase):
                         7: (10.0, 43.0, 54.37), 12: 'K', 13: 0.0, 16: 'T', 17: 246.48684210526315, 23: 'T',
                         24: 246.48684210526315, 29: '2016:06:16', 31: 65.0}, 271: 'Apple', 272: 'iPhone 6',
                 305: '9.3.2', 274: 6, 306: '2016:06:16 12:43:55', 531: 1, 283: 72.0}
-
-        # self.assertDictEqual(data, image.info)
+        
+        # self.assertDictEqual(data, image.info)"""
         item = Store(image.getexif())
         self.assertEqual(item.camera_model, 'iPhone 6')
+        self.assertEqual(item.make, 'Apple')
         self.assertEqual(item.created, datetime(2016, 6, 16, 12, 43, 55))
         self.assertEqual(item.orientation, 6)
         self.assertEqual(item.resolution_unit, 2)
@@ -37,4 +37,16 @@ class TestInfoStore(unittest.TestCase):
         self.assertEqual(item.height, 2448)
         self.assertEqual(item.width, 3264)
         self.assertEqual(item.brightness, 6.454077883908891)
-        # GPSInfo = {1: 'N', 2: (44.0, 49.0, 20.06), 3: 'E', 4: (20.0, 24.0, 38.94), 5: b'\x00', 6: 89.0, 7: (10.0, 43.0, 54.37), 12: 'K', 13: 0.0, 16: 'T', 17: 246.48684210526315, 23: 'T', 24: 246.48684210526315, 29: '2016:06:16', 31: 65.0}
+        self.assertEqual(item.exif_version, b'0221')
+        self.assertEqual(item.f_number, 'N/A', 'but should be f/2,2')
+        self.assertAlmostEqual(item.exposure_time, 1.0 / 175.0, 2)
+        self.assertEqual(item.iso_speed, 0)
+        self.assertEqual(item.metering_mode, 5)
+        self.assertEqual(item.artist, 'N/A')
+        self.assertEqual(item.copyright, 'N/A')
+        self.assertEqual(item.image_description, 'N/A')
+        self.assertEqual(item.user_comment, 'N/A')
+        self.assertAlmostEqual(item.gps_info.longitude, 20, 0)
+        self.assertAlmostEqual(item.gps_info.latitude, 44.8, 1)
+        self.assertEqual(item.gps_info.altitude, 89.0, 'km')
+        # print(item.gps_info)
