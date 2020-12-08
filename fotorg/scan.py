f96101c18bc3fad3b0d07ec859da2ae673059aaf
christoph <chr@buchetmann.de>
2020-12-08 21:32:55 +0100

    start documentation, make private code private

--- fotorg/scan.py
+++ fotorg/scan.py
@@ -1,16 +1,32 @@
 from pathlib import Path, PurePath
-from typing import Iterator, Any, List, Union
+from typing import Iterator, Union
 
 
 class Scan:
-    def __init__(self, directory: Union[PurePath, Path, str] = None, ignore_list: List[str] = None):
+    """Class for setup scanning the folder structure and receive files for considerations."""
+
+    def __init__(self, directory: Union[PurePath, Path, str] = None, ignore_list=None) -> None:
+        """
+        Constructor
+        :param directory:
+        :param ignore_list:
+        """
+        if ignore_list is None:
+            ignore_list = []
         if directory is None:
             self.directory: Path = Path.cwd()
         elif isinstance(directory, Path):
             self.directory: Path = directory
         elif isinstance(directory, str):
             self.directory: Path = Path(directory)
-        self.ignore_list = ignore_list
+        self.__item_counter = 0
+        self.__ignored_dir_list = []
+        self.__ignored_file_list = []
+        for pattern in ignore_list:
+            if pattern.endswith('/'):
+                self.__ignored_dir_list.append(pattern)
+            else:
+                self.__ignored_file_list.append(pattern)
 
     @property
     def items(self) -> Iterator:
