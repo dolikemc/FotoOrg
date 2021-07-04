8a01dfee6b50040239e6fd2110f9d9ba07fcd827
christoph <chr@buchetmann.de>
2021-07-04 15:05:01 +0200

    dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup
 
 setup(
     name='FotoOrg',
-    version='0.0.5',
+    version='0.0.6',
     packages=['fotorg', 'fotorg.info', 'fotorg.info.data'],
     url='https://github.com/dolikemc/',
     license='http://www.apache.org/licenses/LICENSE-2.0',
     author='christoph',
     author_email='dolikemc@gmail.com',
-    description='Organize your Fotos'
+    description='Organize your Fotos',
+    install_requires=[
+        'sqlalchemy>=1.4.20',
+        'sqlalchemy_continuum>=1.3.11',
+        'Pillow>8.3.0',
+        'geopy>=2.1.0',
+    ],
 )
