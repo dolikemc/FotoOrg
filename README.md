629f6ffbb8337a4754baa714f66a2e92b62d14a9
christoph <chr@buchetmann.de>
2021-07-04 10:41:04 +0200

    setup for build tool

---
+++ README.md
@@ -0,0 +1,7 @@
+# Foto Organizer
+## Installation
+todo
+## Basic usage
+todo
+## Bugs and limitations
+todo
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FotoOrg',
+    version='0.0.5',
+    packages=['fotorg', 'fotorg.info', 'fotorg.info.data'],
+    url='https://github.com/dolikemc/',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    author='christoph',
+    author_email='dolikemc@gmail.com',
+    description='Organize your Fotos'
+)
