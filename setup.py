e590531b5e3afe2d8372063c39885bf921e592a6
chris_mit <chr@buchetmann.de>
2021-07-07 15:43:57 +0200

    make tests win10 compatible

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FotoOrg',
-    version='0.0.6',
+    version='0.0.7',
     packages=['fotorg', 'fotorg.info', 'fotorg.info.data'],
     url='https://github.com/dolikemc/',
     license='http://www.apache.org/licenses/LICENSE-2.0',
