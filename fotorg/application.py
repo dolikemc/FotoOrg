1915d91ec1653596bbcbd51be837409b026afffd
christoph <chr@buchetmann.de>
2021-07-16 15:17:28 +0200

    FotoOrg-16 As a designer I want to use an easy to use GUI

--- fotorg/application.py
+++ fotorg/application.py
@@ -1,9 +1,6 @@
 import tkinter as tk
-import os
 
 
 class Application(tk.Tk):
     def __init__(self):
-        if os.environ.get('DISPLAY', '') == '':
-            os.environ['DISPLAY'] = ':0'
         tk.Tk.__init__(self)
